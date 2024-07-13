0652494eda72cf9011c90ad8ec187bde544de070
Anil Roy <anilroy.cce@gmail.com>
2024-07-14 05:05:27 +0530

    Use of While Loop file added

---
+++ decrementState.c
@@ -0,0 +1,15 @@
+/*
+About : This program is for Diaplaying numbers from 10 to 1.
+Used While Loop and Decrement Operator.
+Author: Anil Roy///
+*/
+#include<stdio.h>
+void main(){
+    int i;
+    i=10;
+    while (i>=1)
+    {
+    printf("%d\n", i);
+    i--;
+    }
+}
