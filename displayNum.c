56fdeb4996081e308fbb642a6371db71484d092b
Anil Roy <anilroy.cce@gmail.com>
2024-07-13 08:12:00 +0530

    Used--> While Loop to display NUMBERS

---
+++ displayNum.c
@@ -0,0 +1,17 @@
+
+/*
+About: This Simple  program will print numbers from 1 to 20;
+Used ----> While Loop.
+Author: Anil Roy
+*/
+#include<stdio.h>
+int main(){
+    int i;
+    i=1;
+    while (i<=20)
+    {
+       printf("%d\n", i);
+       i++; 
+    }
+    return 0;
+}
