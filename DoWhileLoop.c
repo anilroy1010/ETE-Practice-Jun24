8ba1330d4a61bc6562faf0cca90841f1aa592708
Anil Roy <anilroy.cce@gmail.com>
2024-07-14 07:53:45 +0530

    Do While Loop Program File

---
+++ DoWhileLoop.c
@@ -0,0 +1,18 @@
+/*
+About: This program will diplay 1 to 100.
+Used Do While Loop.
+Author: Anil Roy
+*/
+#include<stdio.h>
+int main(){
+
+    int i;
+    i=1;
+    do
+    {
+        printf("%d\n", i);
+        i++;
+    } while (i<=100);
+
+    return 0;
+}
