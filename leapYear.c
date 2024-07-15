190c97fe7e065403f42cc49d159fb1fc7c703b51
Anil Roy <anilroy.cce@gmail.com>
2024-07-15 08:03:58 +0530

    Updated LeapYear file

--- leapYear.c
+++ leapYear.c
@@ -7,7 +7,7 @@ int main(){
     int year;
     printf("Enter a year :\n");
     scanf("%d", &year);
-    if ((year%4==0 && year%400 !=0) || ( year%400==0))
+    if ((year%4==0 && year%100 !=0) || ( year%400==0))
     {
         printf("%d is a Leap Year.\n", year);
     }
